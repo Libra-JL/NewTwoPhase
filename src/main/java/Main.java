be3ab537c5bfdca3529f39fef282b6b70835bc34
Libra-JL <1332735161@qq.com>
2021-10-14 00:28:34 +0800

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,31 @@
 import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
-import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.operators.FlatMapOperator;
+import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.util.Collector;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        final DataStreamSource<String> sourceData = env.readTextFile("/Users/libra/Documents/Gitee/NewTwoPhase/src/main/resources/dataset.txt");
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
-        final SingleOutputStreamOperator<String> stringSingleOutputStreamOperator = sourceData.flatMap(new FlatMapFunction<String, String>() {
+        final DataSource<String> sourceData = env.readTextFile("/Users/libra/Documents/Gitee/NewTwoPhase/src/main/resources/dataset.txt").setParallelism(1);
+        final FlatMapOperator<String, String> stringStringFlatMapOperator = sourceData.flatMap(new FlatMapFunction<String, String>() {
             @Override
             public void flatMap(String s, Collector<String> collector) throws Exception {
                 final String[] split = s.split(":");
-                for (String s1 : split) {
-                    collector.collect(s1);
-                }
+                collector.collect(split[0].toString());
             }
-        });
+        }).setParallelism(1);
 
-        stringSingleOutputStreamOperator.print();
 
-        env.execute("test");
+        stringStringFlatMapOperator
+                .writeAsText("/Users/libra/Documents/Gitee/NewTwoPhase/src/main/resources/a.txt", FileSystem.WriteMode.OVERWRITE)
+                .setParallelism(1);
+
+        env.execute();
+
+
+
     }
 }
