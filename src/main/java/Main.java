8bc388de2f23d178cdbd85e6b55dbbaeba8ee66f
Libra-JL <1332735161@qq.com>
2021-10-27 11:10:48 +0800

    加载数据

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,16 +5,37 @@ import org.apache.flink.api.java.operators.FlatMapOperator;
 import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.util.Collector;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+
+
+
         final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
         final DataSource<String> sourceData = env.readTextFile("/Users/libra/Documents/Gitee/NewTwoPhase/src/main/resources/dataset.txt").setParallelism(1);
         final FlatMapOperator<String, String> stringStringFlatMapOperator = sourceData.flatMap(new FlatMapFunction<String, String>() {
             @Override
             public void flatMap(String s, Collector<String> collector) throws Exception {
-                final String[] split = s.split(":");
-                collector.collect(split[0].toString());
+                final String[] splits = s.split(":");
+                final int length = splits[0].split(" ").length;
+                final StringBuilder itemsNum = new StringBuilder();
+                final StringBuilder itemsPrice = new StringBuilder();
+                final Random random = new Random();
+                for (int i = 0; i < length; i++) {
+                    //随机生成项的个数，放在项的个数拼接
+                    itemsNum.append(random.nextInt(100)+50);
+                    itemsNum.append(" ");
+                    itemsPrice.append(random.nextInt(450)+50);
+                    itemsPrice.append(" ");
+                }
+
+                System.out.println(splits[0]+":"+itemsNum.toString()+":"+itemsPrice.toString());
+
+                collector.collect(splits[0]+":"+itemsNum.toString()+":"+itemsPrice.toString());
+//                collector.collect(splits[0]+":"+length);
+
             }
         }).setParallelism(1);
 
